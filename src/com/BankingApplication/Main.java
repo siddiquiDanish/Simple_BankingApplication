80f4ea67d31c72c011e36d7f02569f6c1f5cc726
Dell <Dell@DESKTOP-DN8KRKA>
2020-07-01 03:22:44 +0530

    ArrayList & Autoboxing

---
+++ src/com/BankingApplication/Main.java
@@ -0,0 +1,29 @@
+package com.BankingApplication;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		**SAMPLE**
+//		Bank bank = new Bank("State Bank of India");
+//		bank.addBranch("Jabalpur");
+//		bank.addCustomer("Jabalpur", "Danish", 10800.20);
+//		bank.addCustomer("Jabalpur", "Aman", 20000.98);
+//		bank.addCustomer("Jabalpur", "Aryan", 9000.54);
+//		
+//		bank.addBranch("Pune");
+//		bank.addCustomer("Pune", "Brian", 128000.45);
+//		bank.addCustomer("Pune", "Bob", 15000.24);
+//		
+//		
+//		bank.addCustomerTransaction("Jabalpur", "Danish", 12976.56);
+//		bank.addCustomerTransaction("Jabalpur", "Danish", 146.20);
+//		bank.addCustomerTransaction("Jabalpur", "Danish", 1998.10);
+//		bank.addCustomerTransaction("Jabalpur", "Danish", 2000.34);
+//		
+//		bank.listCustomers("Jabalpur", true);
+//		
+	}
+
+}
